f9a1c359a460511783abda12de9a4067988df4ad
Eddie <86079470+esand68@users.noreply.github.com>
2023-01-28 16:33:38 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+
+let raceNumber = Math.floor(Math.random() * 1000);
+
+
+let earlyRegistered = true;
+
+
+
+let runnerAge = 18;
+
+runnerAge > 18 && earlyRegistered === true ? console.log(`${raceNumber += 1000} is your number and you race at 9:30 AM`)   : 
+runnerAge > 18 && earlyRegistered === false ? console.log(`${raceNumber} is your number and you race at 11:00 am`):
+console.log(`${raceNumber} is your number and you race at 12:30 pm.`)
+
+
+
+
+console.log(raceNumber)
